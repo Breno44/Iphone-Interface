4cbc86257d8d6a1ca9a7548aee24c6fc648f7bbb
Breno Henrique <bhfs.2908@gmail.com>
2021-09-03 13:03:49 -0300

    feat:finish interface

--- src/App.js
+++ src/App.js
@@ -100,6 +100,37 @@ const App = () => {
             </div>
           </div>
         </div>
+        <div className="absolute bottom-8 inset-x-0 -20">
+          <div className="flex justify-between px-10">
+            <button className="bg-gray-800 rounded-full p-2 bg-opacity-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 text-white"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z" />
+              </svg>
+            </button>
+            <button className="bg-gray-800 rounded-full p-2 bg-opacity-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6 text-white"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4 5a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V7a2 2 0 00-2-2h-1.586a1 1 0 01-.707-.293l-1.121-1.121A2 2 0 0011.172 3H8.828a2 2 0 00-1.414.586L6.293 4.707A1 1 0 015.586 5H4zm6 9a3 3 0 100-6 3 3 0 000 6z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="absolute bottom-2 inset-x-0">
+          <div className="h-1 mx-auto w-28 bg-white rounded"></div>
+        </div>
       </div>
     </div>
   );
